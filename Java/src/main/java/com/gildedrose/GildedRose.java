cf5add38b1208f2e9ba2256ae75eece7945d3cd8
LukeBWood <boormanluke76@gmail.com>
2023-11-16 16:37:00 +0000

    Extracted separate items functionality into its own method

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -11,52 +11,71 @@ class GildedRose {
 
         for (Item item : items) {
 
-            boolean isAgedBrie = item.name.equals("Aged Brie");
-
-            if (!item.name.equals("Aged Brie") //items other than aged brie and concert ticket
-                    && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.quality > 0) {
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        item.quality = item.quality - 1;
-                    }
-                }
-            } else { //This is the aged brie condition
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-
-                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                    }
-                } //This condition will never be above 50 according to reqs
+            String itemName = item.name;
+            switch (itemName) {
+                case "Aged Brie" -> updateAgedBrie(item);
+                case "Backstage passes to a TAFKAL80ETC concert" -> updateBackstagePass(item);
+                case "Sulfuras, Hand of Ragnaros" -> updateOtherItems(item);
+                default -> updateOtherItems(item);
             }
 
-            updateSellIn(item);
+            sellByDateReached(item);
+        }
+    }
 
-            if (item.sellIn < 0) {
-                if (!item.name.equals("Aged Brie")) {
-                    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.quality > 0) {
-                            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                                item.quality = item.quality - 1;
-                            }
+    private static void updateOtherItems(Item item) {
+        if (item.quality > 0) {
+            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                item.quality = item.quality - 1;
+            }
+        }
+        updateSellIn(item);
+        sellByDateReached(item);
+    }
+
+    private static void updateAgedBrie(Item item) {
+        if(item.quality < 50) {
+            item.quality++;
+        }
+        updateSellIn(item);
+        sellByDateReached(item);
+    }
+
+    private static void updateBackstagePass(Item item) {
+        if(item.quality < 50) {
+            item.quality++;
+            if (item.sellIn < 11) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                }
+            }
+
+            if (item.sellIn < 6) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                }
+            }
+        }
+        updateSellIn(item);
+        sellByDateReached(item);
+    }
+
+
+    private static void sellByDateReached(Item item) {
+        if (item.sellIn < 0) {
+            if (!item.name.equals("Aged Brie")) {
+                if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (item.quality > 0) {
+                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                            item.quality = item.quality - 1;
                         }
-                    } else {
-                        item.quality = 0;
                     }
                 } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
+                    item.quality = 0;
+                }
+            } else {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
                 }
             }
         }
